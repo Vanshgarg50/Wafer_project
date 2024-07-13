2358ee5d5ad7e816f21c5aed8640f2f2c1751240
vansh <vgavengervansh@gmail.com>
2024-07-14 01:53:09 +0530

    first commit

---
+++ upload_data.py
@@ -0,0 +1,31 @@
+from pymongo.mongo_client import MongoClient
+import pandas as pd
+import json
+
+uri = "mongodb+srv://vanshgarg441:jALrM7C6TL7r4zU3@cluster0.klt4c9w.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+# Create a new client and connect to the server
+#uri = uniform resource identifier
+
+client = MongoClient(uri)
+
+#create database name 
+DATABASE = 'vansh_wafer'
+COLLECTION_NAME = "waferfault"
+
+#dump the data into database
+
+
+# Send a ping to confirm a successful connection
+df = pd.read_csv("D:\Data Science\Wafer_fault_detection_project\Wafer_fault_detection\sensor2-main\notebooks\wafer_23012020_041211.csv")
+df=df.drop("Unnamed: 0",axis=1)
+
+
+json_record=list(json.loads(df.T.to_json()).values())
+
+
+
+
+#dump the data into database
+client[DATABASE][COLLECTION_NAME].insert_many(json_record)
+
+
